9b704e55ce67819cb32021a46c543719f05319ad
oguzhanakcin <oguzhnakcin@gmail.com>
2024-04-25 21:57:48 -0500

    Added colorbar function to swarmvis

--- swarm_visualizer/utility/legendplot_utils.py
+++ swarm_visualizer/utility/legendplot_utils.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.pylab as pylab
+import matplotlib as mpl
+from matplotlib.cm import get_cmap
+import numpy as np
 
 from typing import List, Dict
 from swarm_visualizer.utility import save_fig
@@ -111,3 +114,59 @@ def create_seperate_legend(
 
     # Saves legend
     save_fig(figLegend, save_loc, dpi=600)
+
+def create_colorbar(ax, all_labels, title=None, visible_labels=None, colors=None, palette=None, discrete=False, orientation="vertical"):
+    """Creates a custom colorbar.
+
+    Args:
+        ax: Axis to plot the colorbar
+        all_labels: All labels in the dataset
+        visible_labels: Labels that are visible in the plot
+        colors: Colors of the labels
+        palette: Palette of the colors
+        discrete: If the colorbar is discrete
+        orientation: Orientation of the colorbar
+    """
+
+    # If colors and palette is given at the same time raise an error
+    if colors and palette:
+        raise ValueError("Both colors and palette cannot be given at the same time.")
+
+    # If discrete is False and colors are given raise an error
+    if not discrete and colors:
+        raise ValueError("Colors can only be given if discrete is True.")
+    
+    # If discrete and colors are not given, get the colors from the palette
+    if discrete and not colors:
+        colors = sns.color_palette(palette)
+
+    if discrete:
+        cmaplist = [colors[i] for i in range(len(all_labels))]
+        cmap = mpl.colors.LinearSegmentedColormap.from_list(
+            "Custom cmap", cmaplist, len(cmaplist)
+        )
+        bounds = np.arange(len(all_labels) + 1) - 0.5
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array([])
+        cbar = plt.colorbar(sm, cax=ax, ticks=np.arange(len(all_labels)))
+        
+        if visible_labels:
+
+            # Location of the visible labels
+            visible_labels_location = [all_labels.index(label) for label in visible_labels]
+
+            # Set the ticks and labels
+            cbar.set_ticks(ticks=visible_labels_location, labels=visible_labels)
+        else:
+            cbar.set_ticks(ticks=np.arange(len(all_labels)), labels=all_labels)
+    
+    else:
+        cmap = get_cmap(palette)
+        norm = plt.Normalize(min(all_labels), max(all_labels))
+        cbar = mpl.colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation=orientation)
+        if visible_labels:
+            cbar.set_ticks(ticks=visible_labels)
+    
+    if title:
+        cbar.set_label(title)
