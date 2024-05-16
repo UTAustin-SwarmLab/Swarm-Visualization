bf5f4ece202c6d259f834fb719cabb02c5fdba5b
Po-han Li <34739111+d31003@users.noreply.github.com>
2024-05-16 16:07:46 -0500

    Shorter wilcoxon p value text (#32)
    * Specify dependencies versions
    
    * Fix dependencies versions
    
    * Added a comma in toml

--- swarm_visualizer/utility/statistics_utils.py
+++ swarm_visualizer/utility/statistics_utils.py
@@ -9,11 +9,14 @@ def add_wilcoxon_value(
     hue=None,
     order_list=None,
     box_pairs=None,
-    test_type=None,
-    text_format=None,
+    test_type="Wilcoxon",
+    text_format="full",
     loc=None,
     fontsize=20,
     verbose=0,
+    pvalue_format_string="{:.4f}",
+    show_test_name=False,
+    **kwargs
 ) -> None:
     annotator = Annotator(
         ax, box_pairs, data=df, x=x_var, y=y_var, hue=hue, order=order_list
@@ -24,5 +27,8 @@ def add_wilcoxon_value(
         loc=loc,
         verbose=verbose,
         fontsize=fontsize,
+        pvalue_format_string=pvalue_format_string,
+        show_test_name=show_test_name,
+      **kwargs
     )
     annotator.apply_and_annotate()
